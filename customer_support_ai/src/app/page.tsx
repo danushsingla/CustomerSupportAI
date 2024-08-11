d2381a02acfc7ab9013ffa2c8cfed1a7d77672b6
D-G Singla <dsingla340@gmail.com>
2024-08-10 23:01:50 -0400

    Added API for Claude

--- customer_support_ai/src/app/page.tsx
+++ customer_support_ai/src/app/page.tsx
@@ -18,6 +18,47 @@ export default function Home() {
 
   const [message, setMessage] = useState<string>('');
 
+  const sendMessage = async() => {
+    setMessage('');
+    setMessages((messages) => [
+      ...messages,
+      {
+        role: 'user', content: message},
+        {role: 'assistant', content: ''},
+    ])
+    const response = fetch('/aws', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([...messages, {role: 'user', content: message}]),
+    }).then(async (res) => {
+      const reader = res.body!.getReader()
+      const decoder = new TextDecoder()
+
+      let result = ''
+      // @ts-ignore
+      return reader.read().then(function processText({done, value}) {
+        if (done) {
+          return result
+        }
+        const text = decoder.decode(value || new Int8Array(), {stream: true})
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1]
+          let otherMessages = messages.slice(0, messages.length - 1)
+          return [
+            ...otherMessages,
+            {
+              ...lastMessage,
+              content: lastMessage.content + text,
+            },
+          ]
+        })
+        return reader.read().then(processText)
+      })
+    })
+  }
+
   return (
     <Box
       width={'100%'}
@@ -109,6 +150,7 @@ export default function Home() {
           // variant="contained"
           // onClick={handleSubmit}
           startIcon={<SendIcon />}
+          onClick={sendMessage}
         >
           Send
         </Button>
